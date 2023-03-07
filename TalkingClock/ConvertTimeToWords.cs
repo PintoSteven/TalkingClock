d4e25e61a9814b540ab5c9155c5142fe2e5dd4f4
Steven Pinto <steven.pinto@ymail.com>
2023-03-07 15:17:15 +0000

    Added Code

---
+++ TalkingClock/ConvertTimeToWords.cs
@@ -0,0 +1,36 @@
+﻿namespace TalkingClock
+{
+    public class ConvertTimeToWords
+    {
+        private readonly string[] hours = { "twelve", "one", "two", "three", "four",
+                            "five", "six", "seven", "eight", "nine",
+                            "ten", "eleven"};
+        private readonly string[] minutes = { "", "one", "two", "three", "four",
+                            "five", "six", "seven", "eight", "nine",
+                            "ten", "eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen","twenty","half"};
+        private const int TotalMinutes = 60;
+        public string ConvertToWords(int hour, int minute)
+        {
+            string result = string.Empty;
+            var text = minute > 30 ? "to" : "past";
+            hour = minute > 30 ? (hour + 1) : hour;
+            if (minute == 0)
+            {
+                result = $"{hours[hour % 12]} o'clock";
+            }
+            else
+            {
+                result = $"{ConvertMinute(minute).Trim()} {text} {hours[hour % 12]}";
+            }
+            result = result.Trim();
+            return $"{result.First().ToString().ToUpper()}{String.Join("", result.Skip(1))}";
+        }
+
+        private string ConvertMinute(int minute)
+        {
+            minute = minute > 30 ? TotalMinutes - minute : minute;
+            var remainder = minute % 10;
+            return $"{minutes[(minute == 30 ? minute - 9 : minute) - remainder]} {minutes[remainder]}";
+        }
+    }
+}
--- TalkingClock/Program.cs
+++ TalkingClock/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using TalkingClock;
+
+int Hour;
+int Minute;
+ConvertTimeToWords convertTime = new ConvertTimeToWords();
+if (args.Length > 1)
+{
+    if (args[0].ToLower() != "print")
+    {
+        Console.WriteLine("Wrong Command");
+    }
+    var time = args[1].Split(":").Select(x => x.TrimStart('0')).ToList();
+    if (!int.TryParse(time[0], out Hour) & !int.TryParse(time[1], out Minute))
+    {
+        Console.WriteLine("Wrong Time Input");
+    }
+}
+else
+{
+    Hour = System.DateTime.Now.Hour;
+    Minute = System.DateTime.Now.Minute;
+}
+Console.WriteLine(convertTime.ConvertToWords(Hour, Minute));
+Console.ReadLine();
